64e46f0047944a62e243988217008f0784b14e64
motah-fard <motah.fard@gmail.com>
2022-02-01 23:33:38 -0800

    2

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route} from "react-router-dom"
+import Home from "./components/Home"
+import Nav from "./components/Nav"
+import Projects from "./components/Projects"
+import ContactMe from "./components/ContactMe"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route exact path="/">
+        <Nav />
+        <Home />
+      </Route>
+      <Route path="projects">
+        <Projects/>
+      </Route>
+      <Route path="contact-me">
+        <ContactMe/>
+      </Route>
     </div>
   );
 }
